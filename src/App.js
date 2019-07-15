02da9120aefe905380e2b1d62ba76f42d30952b1
Cory Hedeen <hedeencory@gmail.com>
2019-07-15 17:39:11 -0400

    Build out smaller components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import BottomRow from "./components/BottomRow";
+import TopRow from "./components/TopRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -10,24 +11,13 @@ const [score, setScore] = useState(32);
 const [awayScore, setAwayScore] = useState(32);
 
   return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{score}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+      <div>
+        <div className="container">
+          <section className="scoreboard">
+            <TopRow />
+            <BottomRow />
+          </section>
         </div>
-        <BottomRow />
-      </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
